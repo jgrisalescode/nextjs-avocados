5511d44d9ce8ee8c2d07a776c33c4a9ddce32db0
Julian Grisales <jgrisales@gmail.com>
2021-11-19 23:03:22 +0100

    CSS Solutions - Global CSS

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "@components/Navbar/Navbar"
 
 const Layout: React.FC = ({children}) => {
     return (
-        <div>
+        <div className="container">
             <Navbar />
             {children}
             <footer>This is the footer</footer>
