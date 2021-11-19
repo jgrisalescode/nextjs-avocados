b93abf6bd52337f6749d39b566c431def2ca0473
Julian Grisales <jgrisales@gmail.com>
2021-11-19 23:09:54 +0100

    CSS Solutions - Module CSS

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,8 +1,11 @@
 import Navbar from "@components/Navbar/Navbar"
+import styles from './layout.module.css'
+// CSS Module
+// https://nextjs.org/docs/basic-features/built-in-css-support
 
 const Layout: React.FC = ({children}) => {
     return (
-        <div className="container">
+        <div className={styles.container}>
             <Navbar />
             {children}
             <footer>This is the footer</footer>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app'
 import Layout from '@components/Layout/Layout'
-import '../style.css'
 
 function MyApp({ Component, pageProps }): AppProps {
     // Uses:
