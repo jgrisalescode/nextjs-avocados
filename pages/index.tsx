fca987e194c16a3486bbc0babe6387cd98043aef
Julian Grisales <jgrisales@gmail.com>
2021-11-28 21:20:05 +0100

    getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,27 @@
-import { useState, useEffect } from 'react'
+import React from 'react'
+import fetch from 'isomorphic-unfetch'
 import Layout from '@components/Layout/Layout'
 import KawaiHeader from '@components/KawaiHeader/KawaiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const Home = () => {
-  const [productsList, setProductsList] = useState<TProduct[]>([])
-
-  useEffect(() => {
-    window
-      .fetch('/api/avocados')
-      .then(response => response.json())
-      .then(({ data, length }) => {
-        setProductsList(data)
-      })
-  }, [])
-
+const Home = ({ products } : { products: TProduct[]}) => {
   return (
     <Layout>
       <KawaiHeader />
-      <ProductList products={productsList}/>
+      <ProductList products={products}/>
     </Layout>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('https://jgrisalescode-nextjs-avocados.vercel.app/api/avocados')
+  const {data}: TAPIAvoResponse = await response.json()
+
+  return {
+    props: {
+      products: data
+    }
+  }
 }
 
 export default Home
